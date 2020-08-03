192ec5c29828baaae73a3593178c85abd00f4a7a
Yaroslav <55299766+BLUE-OYSTER@users.noreply.github.com>
2020-08-03 13:54:07 +0300

    Updates

---
+++ precache-manifest.e71d6566771b94875d97dfc2700149f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b6ab82c127745eded11",
+    "url": "/react-with-ts-learning-project/static/css/main.724317d0.chunk.css"
+  },
+  {
+    "revision": "7b6ab82c127745eded11",
+    "url": "/react-with-ts-learning-project/static/js/main.c7b637ac.chunk.js"
+  },
+  {
+    "revision": "488f6f89d36684fc636d",
+    "url": "/react-with-ts-learning-project/static/js/runtime~main.8c41d973.js"
+  },
+  {
+    "revision": "2dc7951ef6be3e5e01f3",
+    "url": "/react-with-ts-learning-project/static/css/2.68070f80.chunk.css"
+  },
+  {
+    "revision": "2dc7951ef6be3e5e01f3",
+    "url": "/react-with-ts-learning-project/static/js/2.a63fbebc.chunk.js"
+  },
+  {
+    "revision": "51bdba48331e1c5e39ab417597357842",
+    "url": "/react-with-ts-learning-project/index.html"
+  }
+];
